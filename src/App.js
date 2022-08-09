1d5c43be83de6382ff5253038d6f985897d198ef
skapoor68 <skapoor68@gatech.edu>
2022-08-09 11:44:05 -0400

    create front end layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Center, Text, Input, HStack, Button} from '@chakra-ui/react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box h='calc(100vh)' backgroundColor = 'black'>
+      <Center h='100px'>
+        <Text fontSize='7xl' color='#1DB954' fontWeight='semibold' marginTop='5' textShadow='1px 1px white'>
+          Featurefy
+        </Text>
+      </Center>
+      <Box>
+        <Center h='100px' color='white'>
+          <Text fontSize='2xl'>
+            Find the shortest path from
+          </Text>
+        </Center>
+        <Box>
+          <Center h='75px'>
+            <HStack spacing='5'>
+              <Input placeholder='Kanye West' size='lg' color='white'/>
+              <Text fontSize='xl' color='white'>
+                To
+              </Text>
+              <Input placeholder='Taylor Swift' size='lg' color='white'/>
+            </HStack>
+          </Center>
+        </Box>
+        <Box>
+          <Center h='100px'> 
+            <Button color='#1DB954' size='lg'>
+              Search
+            </Button>
+          </Center>
+        </Box>
+      </Box>
+    </Box>
+
+
   );
 }
 
